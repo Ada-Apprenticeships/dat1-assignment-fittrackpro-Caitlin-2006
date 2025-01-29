19baaada83d3ee12320e1d963a95fffff43b0542
Caitlin-2006 <caitlin.ohare@ada.ac.uk>
2025-01-29 21:03:48 +0000

    beginning of user management queries

--- src/user_management.sql
+++ src/user_management.sql
@@ -8,15 +8,21 @@ PRAGMA foreign_keys = ON;
 
 -- 1. Retrieve all members
 -- TODO: Write a query to retrieve all members
+SELECT * FROM members;
 
 -- 2. Update a member's contact information
 -- TODO: Write a query to update a member's contact information
 
 -- 3. Count total number of members
 -- TODO: Write a query to count the total number of members
+SELECT COUNT(*) AS total_members FROM members;
 
 -- 4. Find member with the most class registrations
 -- TODO: Write a query to find the member with the most class registrations
+SELECT member_id, COUNT(*) AS registration_count
+FROM class_attendance
+GROUP BY member_id
+ORDER BY registration_count DESC
 
 -- 5. Find member with the least class registrations
 -- TODO: Write a query to find the member with the least class registrations
