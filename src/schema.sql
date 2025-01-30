49506e6dde546d0fd7c61dd71a9b06ff1f566f6b
Caitlin-2006 <caitlin.ohare@ada.ac.uk>
2025-01-30 12:54:38 +0000

    text to varchar

--- src/schema.sql
+++ src/schema.sql
@@ -46,140 +46,126 @@ DROP TABLE IF EXISTS equipment_maintenance_log;
 
 CREATE TABLE locations (
     location_id INTEGER PRIMARY KEY,
-    name TEXT NOT NULL,
-    address TEXT NOT NULL,
-    phone_number TEXT,
-    email TEXT,
-    opening_hours TEXT
-);
+    name VARCHAR(100) NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    phone_number VARCHAR(15) NOT NULL CHECK(phone_number LIKE '%-%'),
+    email VARCHAR(100) NOT NULL CHECK(email LIKE '%@%'),
+    opening_hours VARCHAR(50) NOT NULL);
 
 CREATE TABLE members (
     member_id INTEGER PRIMARY KEY,
-    first_name TEXT NOT NULL,
-    last_name TEXT NOT NULL,
-    email TEXT,
-    phone_number TEXT,
-    date_of_birth DATE,
-    join_date DATE,
-    emergency_contact_name TEXT,
-    emergency_contact_phone TEXT
-);
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    phone_number VARCHAR(15) NOT NULL CHECK(phone_number LIKE '%-%'),
+    date_of_birth DATE NOT NULL,
+    join_date DATE NOT NULL,
+    emergency_contact_name VARCHAR(100) NOT NULL,
+    emergency_contact_phone VARCHAR(15) NOT NULL);
 
 CREATE TABLE staff (
     staff_id INTEGER PRIMARY KEY,
-    first_name TEXT NOT NULL,
-    last_name TEXT NOT NULL,
-    email TEXT,
-    phone_number TEXT,
-    position TEXT CHECK(position IN ('Manager', 'Trainer', 'Receptionist','Maintenance')),
-    hire_date DATE,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    phone_number VARCHAR(15) NOT NULL CHECK(phone_number LIKE '%-%'),
+    position VARCHAR(20) CHECK(position IN ('Trainer', 'Manager', 'Receptionist', 'Maintenance')) NOT NULL,
+    hire_date DATE NOT NULL,
     location_id INTEGER,
-    FOREIGN KEY (location_id) REFERENCES locations(location_id)
-);
+    FOREIGN KEY (location_id) REFERENCES locations(location_id));
 
 CREATE TABLE equipment (
     equipment_id INTEGER PRIMARY KEY,
-    name TEXT NOT NULL,
-    type TEXT,
-    purchase_date DATE,
+    name VARCHAR(100) NOT NULL,
+    type VARCHAR(20) CHECK(type IN ('Cardio', 'Strength')) NOT NULL,
+    purchase_date DATE NOT NULL,
     last_maintenance_date DATE,
     next_maintenance_date DATE,
     location_id INTEGER,
-    FOREIGN KEY (location_id) REFERENCES locations(location_id)
-);
+    FOREIGN KEY (location_id) REFERENCES locations(location_id));
 
 CREATE TABLE classes (
     class_id INTEGER PRIMARY KEY,
-    name TEXT NOT NULL,
+    name VARCHAR(100) NOT NULL,
     description TEXT,
-    capacity INTEGER,
-    duration INTEGER,
+    capacity INTEGER CHECK(capacity > 0) NOT NULL,
+    duration INTEGER CHECK(duration > 0) NOT NULL,
     location_id INTEGER,
-    FOREIGN KEY (location_id) REFERENCES locations(location_id)
-);
+    FOREIGN KEY (location_id) REFERENCES locations(location_id));
 
 CREATE TABLE class_schedule (
     schedule_id INTEGER PRIMARY KEY,
     class_id INTEGER,
     staff_id INTEGER,
-    start_time DATETIME,
-    end_time DATETIME,
+    start_time DATETIME NOT NULL,
+    end_time DATETIME NOT NULL,
     FOREIGN KEY (class_id) REFERENCES classes(class_id),
-    FOREIGN KEY (staff_id) REFERENCES staff(staff_id)
-);
+    FOREIGN KEY (staff_id) REFERENCES staff(staff_id));
 
 CREATE TABLE memberships (
     membership_id INTEGER PRIMARY KEY,
     member_id INTEGER,
-    type TEXT,
-    start_date DATE,
-    end_date DATE,
-    status TEXT,
-    FOREIGN KEY (member_id) REFERENCES members(member_id)
-);
+    type VARCHAR(50) NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    status VARCHAR(10) CHECK(status IN ('Active', 'Inactive')) NOT NULL,
+    FOREIGN KEY (member_id) REFERENCES members(member_id));
 
 CREATE TABLE attendance (
     attendance_id INTEGER PRIMARY KEY,
     member_id INTEGER,
     location_id INTEGER,
-    check_in_time DATETIME,
+    check_in_time DATETIME NOT NULL,
     check_out_time DATETIME,
     FOREIGN KEY (member_id) REFERENCES members(member_id),
-    FOREIGN KEY (location_id) REFERENCES locations(location_id)
-);
+    FOREIGN KEY (location_id) REFERENCES locations(location_id));
 
 CREATE TABLE class_attendance (
     class_attendance_id INTEGER PRIMARY KEY,
     schedule_id INTEGER,
     member_id INTEGER,
-    attendance_status TEXT,
+    attendance_status VARCHAR(15) CHECK(attendance_status IN ('Registered', 'Attended', 'Unattended')) NOT NULL,
     FOREIGN KEY (schedule_id) REFERENCES class_schedule(schedule_id),
-    FOREIGN KEY (member_id) REFERENCES members(member_id)
-);
+    FOREIGN KEY (member_id) REFERENCES members(member_id));
 
 CREATE TABLE payments (
     payment_id INTEGER PRIMARY KEY,
     member_id INTEGER,
-    amount REAL,
-    payment_date DATE,
-    payment_method TEXT ,
-    payment_type TEXT,
-    FOREIGN KEY (member_id) REFERENCES members(member_id)
-);
+    amount DECIMAL(10, 2) CHECK(amount > 0) NOT NULL,
+    payment_date DATETIME NOT NULL,
+    payment_method VARCHAR(20) CHECK(payment_method IN ('Credit Card', 'Bank Transfer', 'PayPal', 'Cash')) NOT NULL,
+    payment_type VARCHAR(50) CHECK(payment_type IN ('Monthly membership fee', 'Day pass')) NOT NULL,
+    FOREIGN KEY (member_id) REFERENCES members(member_id));
 
 CREATE TABLE personal_training_sessions (
     session_id INTEGER PRIMARY KEY,
     member_id INTEGER,
     staff_id INTEGER,
-    session_date DATE,
-    start_time TIME,
-    end_time TIME,
+    session_date DATE NOT NULL,
+    start_time TIME NOT NULL,
+    end_time TIME NOT NULL,
     notes TEXT,
     FOREIGN KEY (member_id) REFERENCES members(member_id),
-    FOREIGN KEY (staff_id) REFERENCES staff(staff_id)
-);
+    FOREIGN KEY (staff_id) REFERENCES staff(staff_id));
 
 CREATE TABLE member_health_metrics (
     metric_id INTEGER PRIMARY KEY,
     member_id INTEGER,
-    measurement_date DATE,
-    weight REAL,
-    body_fat_percentage REAL,
-    muscle_mass REAL,
-    bmi REAL,
-    FOREIGN KEY (member_id) REFERENCES members(member_id)
-);
+    measurement_date DATE NOT NULL,
+    weight DECIMAL(5, 2) CHECK(weight > 0) NOT NULL,
+    body_fat_percentage DECIMAL(5, 2) CHECK(body_fat_percentage >= 0 AND body_fat_percentage <= 100) NOT NULL,
+    muscle_mass DECIMAL(5, 2) CHECK(muscle_mass > 0) NOT NULL,
+    bmi DECIMAL(5, 2) CHECK(bmi > 0) NOT NULL,
+    FOREIGN KEY (member_id) REFERENCES members(member_id));
 
 CREATE TABLE equipment_maintenance_log (
     log_id INTEGER PRIMARY KEY,
     equipment_id INTEGER,
-    maintenance_date DATE,
-    description TEXT,
+    maintenance_date DATE NOT NULL,
+    description TEXT NOT NULL,
     staff_id INTEGER,
     FOREIGN KEY (equipment_id) REFERENCES equipment(equipment_id),
-    FOREIGN KEY (staff_id) REFERENCES staff(staff_id)
-);
-
+    FOREIGN KEY (staff_id) REFERENCES staff(staff_id));
 
 -- After creating the tables, you can import the sample data using:
 -- `.read data/sample_data.sql` in a sql file or `npm run import` in the terminal
